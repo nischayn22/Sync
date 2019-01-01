3dd049a72ac3c664508df1a67a3c9a8b360fa1ed
Nischayn22 <nischayn22@gmail.com>
2019-01-01 13:44:50 +0530

    aded pagelist option

--- maintenance/importWiki.php
+++ maintenance/importWiki.php
@@ -15,6 +15,12 @@ class ImportWiki extends Maintenance {
 		parent::__construct();
 
 		$this->mDescription = "Bulk Imports this wiki to other wikis.";
+		$this->addOption(
+			'pagelist',
+			'Comma-separated list of pages to be imported',
+			false,
+			true
+		);
 	}
 
 	public function execute() {
@@ -23,21 +29,36 @@ class ImportWiki extends Maintenance {
 		$dbr = wfGetDB( DB_SLAVE );
 		foreach ( $wgSyncWikis as $wgSyncWiki ) {
 			$pages = array();
-			if( count( $wgSyncWiki['copy_ns'] ) > 0 ) {
-				foreach( $wgSyncWiki['copy_ns'] as $namespaceId ) {
-					$conds = [ 'page_namespace' => $namespaceId, 'page_is_redirect' => 0 ];
-					$res = $dbr->select( 'page',
-						[ 'page_title', 'page_id' ],
-						$conds,
-						__METHOD__
-					);
-					foreach( $res as $row ) {
-						$pages[$row->page_id] = $row->page_title;
+			if ( $this->hasOption( 'pagelist' ) ) {
+				$pagelist = $this->getOption( 'pagelist' );
+				if ( $pagelist !== '' ) {
+					$pagelist = explode( ',', $pagelist );
+				}
+				foreach( $pagelist as $page ) {
+					$page_id = WikiPage::factory( Title::newFromText( $page ) )->getId();
+					if ( is_null( $page_id ) ) {
+						$this->error( "Could not find page_id for " . $page );
+						return;
+					}
+					$pages[$page_id] = $page;
+				}
+			} else {
+				if( count( $wgSyncWiki['copy_ns'] ) > 0 ) {
+					foreach( $wgSyncWiki['copy_ns'] as $namespaceId ) {
+						$conds = [ 'page_namespace' => $namespaceId, 'page_is_redirect' => 0 ];
+						$res = $dbr->select( 'page',
+							[ 'page_title', 'page_id' ],
+							$conds,
+							__METHOD__
+						);
+						foreach( $res as $row ) {
+							$pages[$row->page_id] = $row->page_title;
+						}
 					}
 				}
 			}
 			$syncWiki = new MediaWikiApi( $wgSyncWiki['api_path'] );
-			echo "Logging in to sync wiki\n";
+			echo "Logging in to sync wiki: " . $wgSyncWiki['api_path'] . "\n";
 			$syncWiki->logout();
 			if ( $syncWiki->login( $wgSyncWiki['username'], $wgSyncWiki['password'] ) ) {
 				echo "Successfully logged in\n";
@@ -47,6 +68,7 @@ class ImportWiki extends Maintenance {
 
 			foreach( $pages as $pageid => $pageName ) {
 				if ( $wgSyncWiki['translate'] ) {
+					echo "Translating title " . $pageName . " to ". $wgSyncWiki['translate_to'] ."\n";
 					$title = $autoTranslate->translateTitle( $pageid );
 					$content = $autoTranslate->translate( $pageid );
 				} else {
@@ -54,6 +76,7 @@ class ImportWiki extends Maintenance {
 					$content = ContentHandler::getContentText( $revision->getContent( Revision::RAW ) );
 					$title = $pageName;
 				}
+				echo "Syncing title " . $pageName . "\n";
 				$data = $syncWiki->editPage( $title, $content );
 				if ( $data ) {
 					echo "Synced $pageName as $title\n";
